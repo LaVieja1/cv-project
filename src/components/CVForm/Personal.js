db9a7d02f7bb489df1173ba7a653a79e6166364a
Santi Cano <canosantiago404@gmail.com>
2021-10-24 15:26:00 -0300

    fixed photo file input

--- src/components/CVForm/Personal.js
+++ src/components/CVForm/Personal.js
@@ -5,68 +5,68 @@ import TextArea from "../Utils/TextArea";
 import FileInput from "../Utils/FileInput";
 
 const Personal = ({ personalInfo, onChange }) => {
-    return (
-        <Section
-            title="Información personal"
-            titlePadding="0.5rem"
-            direction="column"
-        >
-            <Input
-                onChange={(e) => onChange(e)}
-                type="text"
-                name="firstName"
-                placeholder="Nombre"
-                value={personalInfo.firstName}
-            />
-            <Input
-                onChange={(e) => onChange(e)}
-                type="text"
-                name="lastName"
-                placeholder="Apellido"
-                value={personalInfo.lastName}
-            />
-            <Input
-                onChange={(e) => onChange(e)}
-                type="text"
-                name="title"
-                placeholder="Título"
-                value={personalInfo.title}
-            />
-            <Input
-                onChange={(e) => onChange(e)}
-                type="photo"
-                label="Foto"
-                value={personalInfo.photo}
-            />
-            <Input
-                onChange={(e) => onChange(e)}
-                type="text"
-                name="address"
-                placeholder="Dirección"
-                value={personalInfo.address}
-            />
-            <Input
-                onChange={(e) => onChange(e)}
-                type="text"
-                name="phoneNumber"
-                placeholder="Numero de teléfono"
-                value={personalInfo.phoneNumber}
-            />
-            <Input
-                onChange={(e) => onChange(e)}
-                type="text"
-                name="email"
-                placeholder="Email"
-                value={personalInfo.email}
-            />
-            <TextArea
-                onChange={(e) => onChange(e)}
-                name="description"
-                placeholder="Descripción"
-                value={personalInfo.description}
-            />
-        </Section>
-    );
+  return (
+    <Section
+      title="Informacion personal"
+      titlePadding="0.5rem"
+      direction="column"
+    >
+      <Input
+        onChange={(e) => onChange(e)}
+        type="text"
+        name="firstName"
+        placeholder="Nombre"
+        value={personalInfo.firstName}
+      />
+      <Input
+        onChange={(e) => onChange(e)}
+        type="text"
+        name="lastName"
+        placeholder="Apellido"
+        value={personalInfo.lastName}
+      />
+      <Input
+        onChange={(e) => onChange(e)}
+        type="text"
+        name="title"
+        placeholder="Título"
+        value={personalInfo.title}
+      />
+      <FileInput
+        onChange={(e) => onChange(e)}
+        name="photo"
+        label="Foto"
+        value={personalInfo.photo}
+      />
+      <Input
+        onChange={(e) => onChange(e)}
+        type="text"
+        name="address"
+        placeholder="Dirección"
+        value={personalInfo.address}
+      />
+      <Input
+        onChange={(e) => onChange(e)}
+        type="text"
+        name="phoneNumber"
+        placeholder="Numero de teléfono"
+        value={personalInfo.phoneNumber}
+      />
+      <Input
+        onChange={(e) => onChange(e)}
+        type="text"
+        name="email"
+        placeholder="Email"
+        value={personalInfo.email}
+      />
+      <TextArea
+        onChange={(e) => onChange(e)}
+        name="description"
+        placeholder="Descripción"
+        value={personalInfo.description}
+      />
+    </Section>
+  );
 };
 
 export default Personal;
